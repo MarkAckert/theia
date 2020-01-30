dff68abdd102a8173eb6b5904609571ba4d1699d
MarkAckert <mark.ackert@broadcom.com>
2020-01-30 14:43:59 -0500

    remove dialog per issue 6845

--- packages/filesystem/src/browser/filesystem-frontend-module.ts
+++ packages/filesystem/src/browser/filesystem-frontend-module.ts
@@ -14,22 +14,19 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-import '../../src/browser/style/index.css';
-
+import { FrontendApplicationContribution, WebSocketConnectionProvider } from '@theia/core/lib/browser';
+import { CommandContribution, ResourceResolver } from '@theia/core/lib/common';
 import { ContainerModule, interfaces } from 'inversify';
-import { ResourceResolver, CommandContribution } from '@theia/core/lib/common';
-import { WebSocketConnectionProvider, FrontendApplicationContribution, ConfirmDialog } from '@theia/core/lib/browser';
-import { FileSystem, fileSystemPath, FileShouldOverwrite, FileStat } from '../common';
-import {
-    fileSystemWatcherPath, FileSystemWatcherServer,
-    FileSystemWatcherServerProxy, ReconnectingFileSystemWatcherServer
-} from '../common/filesystem-watcher-protocol';
+import '../../src/browser/style/index.css';
+import { FileSystem, fileSystemPath } from '../common';
+import { fileSystemWatcherPath, FileSystemWatcherServer, FileSystemWatcherServerProxy, ReconnectingFileSystemWatcherServer } from '../common/filesystem-watcher-protocol';
 import { FileResourceResolver } from './file-resource';
-import { bindFileSystemPreferences } from './filesystem-preferences';
-import { FileSystemWatcher } from './filesystem-watcher';
-import { FileSystemFrontendContribution } from './filesystem-frontend-contribution';
-import { FileSystemProxyFactory } from './filesystem-proxy-factory';
 import { FileUploadService } from './file-upload-service';
+import { FileSystemFrontendContribution } from './filesystem-frontend-contribution';
+import { bindFileSystemPreferences } from './filesystem-preferences';
+import { FileSystemProxyFactory } from './filesystem-proxy-factory';
+import { FileSystemWatcher } from './filesystem-watcher';
+
 
 export default new ContainerModule(bind => {
     bindFileSystemPreferences(bind);
@@ -39,15 +36,6 @@ export default new ContainerModule(bind => {
     );
     bind(FileSystemWatcherServer).to(ReconnectingFileSystemWatcherServer);
     bind(FileSystemWatcher).toSelf().inSingletonScope();
-    bind(FileShouldOverwrite).toFunction(async function (file: FileStat, stat: FileStat): Promise<boolean> {
-        const dialog = new ConfirmDialog({
-            title: `The file '${file.uri}' has been changed on the file system.`,
-            msg: 'Do you want to overwrite the changes made on the file system?',
-            ok: 'Yes',
-            cancel: 'No'
-        });
-        return !!await dialog.open();
-    });
 
     bind(FileSystemProxyFactory).toSelf();
     bind(FileSystem).toDynamicValue(ctx => {
